8ac5c7523bb0b25ad03b6da249169d46556f27f0
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-12-13 14:27:03 +0300

    retreiving data

---
+++ lib/views/detail_country_info.dart
@@ -0,0 +1,161 @@
+import 'package:covid_19/model/covid_model.dart';
+import 'package:covid_19/service/covid_service.dart';
+import 'package:flutter/material.dart';
+
+class DetailCountryInfo extends StatelessWidget {
+  List countryFlagList = [
+    "assets/images/algeria.png",
+    "assets/images/austria.png",
+    "assets/images/azerbaijan.png",
+    "assets/images/bahrain.png",
+    "assets/images/belgium.png",
+    "assets/images/brazil.png",
+    "assets/images/bulgaria.png",
+    "assets/images/canada.png",
+    "assets/images/china.png",
+    "assets/images/czech-republic.png",
+    "assets/images/denmark.png",
+    "assets/images/finland.png",
+    "assets/images/france.png",
+    "assets/images/germany.png",
+    "assets/images/honduras.png",
+    "assets/images/hungary.png",
+    "assets/images/india.png",
+    "assets/images/iran.png",
+    "assets/images/italy.png",
+    "assets/images/japan.png",
+    "assets/images/luxembourg.png",
+    "assets/images/malaysia.png",
+    "assets/images/netherlands.png",
+    "assets/images/nigeria.png",
+    "assets/images/norway.png",
+    "assets/images/pakistan.png",
+    "assets/images/palestine.png",
+    "assets/images/philippines.png",
+    "assets/images/poland.png",
+    "assets/images/portugal.png",
+    "assets/images/romania.png",
+    "assets/images/russia.png",
+    "assets/images/saudi-arabia.png",
+    "assets/images/serbia.png",
+    "assets/images/singapore.png",
+    "assets/images/slovakia.png",
+    "assets/images/slovenia.png",
+    "assets/images/south-korea.png",
+    "assets/images/spain.png",
+    "assets/images/sweden.png",
+    "assets/images/switzerland.png",
+    "assets/images/turkey.png",
+    "assets/images/england.png",
+    "assets/images/united-states.png",
+    "assets/images/vietnam.png",
+  ];
+
+  String imageUrl;
+  String country;
+  dynamic infected;
+  dynamic tested;
+  dynamic recovered;
+  dynamic deceased;
+
+  DetailCountryInfo(
+      {required this.country,
+      required this.imageUrl,
+      required this.infected,
+      this.tested,
+      this.recovered,
+      this.deceased});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              Container(height: 200, child: Image.asset(imageUrl)),
+              Center(child: Text(country)),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 85,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                        color: Colors.pink,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text("Infected"),
+                        Text('$infected'),
+                      ],
+                    )),
+                  ),
+                  Container(
+                    height: 85,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                        color: Colors.blueAccent,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text("Recovered"),
+                        Text('$recovered'),
+                      ],
+                    )),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    height: 85,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text('Tested'),
+                        Text('$tested'),
+                      ],
+                    )),
+                  ),
+                  Container(
+                    height: 85,
+                    width: MediaQuery.of(context).size.width * 0.45,
+                    decoration: BoxDecoration(
+                        color: Colors.amber,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Center(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        const Text('Deceased'),
+                        Text('$deceased'),
+                      ],
+                    )),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,56 +1,57 @@
 import 'package:covid_19/model/covid_model.dart';
 import 'package:covid_19/service/covid_service.dart';
-import 'package:covid_19/widget/custom_container.dart';
+import 'package:covid_19/views/detail_country_info.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   CovidService covidService = CovidService();
 
   List countryFlagList = [
-    Image.asset("assets/images/algeria.png"),
-    Image.asset("assets/images/austria.png"),
-    Image.asset("assets/images/azerbaijan.png"),
-    Image.asset("assets/images/bahrain.png"),
-    Image.asset("assets/images/belgium.png"),
-    Image.asset("assets/images/brazil.png"),
-    Image.asset("assets/images/bulgaria.png"),
-    Image.asset("assets/images/canada.png"),
-    Image.asset("assets/images/china.png"),
-    Image.asset("assets/images/czech-republic.png"),
-    Image.asset("assets/images/denmark.png"),
-    Image.asset("assets/images/england.png"),
-    Image.asset("assets/images/finland.png"),
-    Image.asset("assets/images/france.png"),
-    Image.asset("assets/images/germany.png"),
-    Image.asset("assets/images/honduras.png"),
-    Image.asset("assets/images/india.png"),
-    Image.asset("assets/images/iran.png"),
-    Image.asset("assets/images/italy.png"),
-    Image.asset("assets/images/japan.png"),
-    Image.asset("assets/images/luxembourg.png"),
-    Image.asset("assets/images/malaysia.png"),
-    Image.asset("assets/images/netherlands.png"),
-    Image.asset("assets/images/nigeria.png"),
-    Image.asset("assets/images/norway.png"),
-    Image.asset("assets/images/pakistan.png"),
-    Image.asset("assets/images/palestine.png"),
-    Image.asset("assets/images/philippines.png"),
-    Image.asset("assets/images/poland.png"),
-    Image.asset("assets/images/portugal.png"),
-    Image.asset("assets/images/romania.png"),
-    Image.asset("assets/images/russia.png"),
-    Image.asset("assets/images/saudi-arabia.png"),
-    Image.asset("assets/images/serbia.png"),
-    Image.asset("assets/images/singapore.png"),
-    Image.asset("assets/images/slovakia.png"),
-    Image.asset("assets/images/slovenia.png"),
-    Image.asset("assets/images/south-korea.png"),
-    Image.asset("assets/images/spain.png"),
-    Image.asset("assets/images/sweden.png"),
-    Image.asset("assets/images/switzerland.png"),
-    Image.asset("assets/images/turkey.png"),
-    Image.asset("assets/images/united-states.png"),
-    Image.asset("assets/images/vietnam.png"),
+    "assets/images/algeria.png",
+    "assets/images/austria.png",
+    "assets/images/azerbaijan.png",
+    "assets/images/bahrain.png",
+    "assets/images/belgium.png",
+    "assets/images/brazil.png",
+    "assets/images/bulgaria.png",
+    "assets/images/canada.png",
+    "assets/images/china.png",
+    "assets/images/czech-republic.png",
+    "assets/images/denmark.png",
+    "assets/images/finland.png",
+    "assets/images/france.png",
+    "assets/images/germany.png",
+    "assets/images/honduras.png",
+    "assets/images/hungary.png",
+    "assets/images/india.png",
+    "assets/images/iran.png",
+    "assets/images/italy.png",
+    "assets/images/japan.png",
+    "assets/images/luxembourg.png",
+    "assets/images/malaysia.png",
+    "assets/images/netherlands.png",
+    "assets/images/nigeria.png",
+    "assets/images/norway.png",
+    "assets/images/pakistan.png",
+    "assets/images/palestine.png",
+    "assets/images/philippines.png",
+    "assets/images/poland.png",
+    "assets/images/portugal.png",
+    "assets/images/romania.png",
+    "assets/images/russia.png",
+    "assets/images/saudi-arabia.png",
+    "assets/images/serbia.png",
+    "assets/images/singapore.png",
+    "assets/images/slovakia.png",
+    "assets/images/slovenia.png",
+    "assets/images/south-korea.png",
+    "assets/images/spain.png",
+    "assets/images/sweden.png",
+    "assets/images/switzerland.png",
+    "assets/images/turkey.png",
+    "assets/images/england.png",
+    "assets/images/united-states.png",
+    "assets/images/vietnam.png",
   ];
 
   @override
@@ -77,29 +78,33 @@ class HomeScreen extends StatelessWidget {
               shrinkWrap: true,
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                return Container(
-                  width: MediaQuery.of(context).size.width * 0.85,
-                  height: 75,
-                  child: Card(
-                      child: ListTile(
-                    leading: Container(
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => DetailCountryInfo(
+                          country: snapshot.data![index].country!,
+                          imageUrl: countryFlagList[index],
+                          infected: snapshot.data![index].infected,
+                          tested: snapshot.data![index].tested,
+                          recovered: snapshot.data![index].recovered,
+                          deceased: snapshot.data![index].deceased),
+                    ));
+                  },
+                  child: Container(
+                    width: MediaQuery.of(context).size.width * 0.85,
+                    height: 75,
+                    child: Card(
+                        child: ListTile(
+                      leading: Container(
                         width: 50,
                         height: 50,
-                        child: Image.asset(
-                          "assets/images/${countryFlagList.map((e) => e)}.png",
-                          // countryFlagList.map((e) => e).toString(),
-                          fit: BoxFit.fill,
-                        )),
-                    title: Text(snapshot.data![index].country!),
-                    trailing: Text(snapshot.data![index].infected.toString()),
-                  )),
+                        child: Image(image: AssetImage(countryFlagList[index])),
+                      ),
+                      title: Text(snapshot.data![index].country!),
+                      trailing: Text(snapshot.data![index].infected.toString()),
+                    )),
+                  ),
                 );
-
-                // return CustomContainer(
-                //   countryFlagList: countryFlagList,
-                //   title: snapshot.data![index].country!,
-                //   infected: snapshot.data![index].infected.toString(),
-                // );
               },
             );
           }),
