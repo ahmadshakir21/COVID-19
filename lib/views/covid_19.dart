2a932fde0f397b15aa5904a6e569a1a194c4cb8c
ahmadshakir21 <ahmad.software.developer@gmail.com>
2022-12-17 16:38:57 +0300

    another commit

--- lib/views/covid_19.dart
+++ lib/views/covid_19.dart
@@ -1,6 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:covid_19/model/covid_19_dummy_data.dart';
 import 'package:covid_19/widget/top_of_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class Covid19 extends StatelessWidget {
   const Covid19({Key? key}) : super(key: key);
@@ -17,11 +19,31 @@ class Covid19 extends StatelessWidget {
           children: [
             TopOfScreen(
                 iconData: Icons.arrow_back_rounded,
-                title: "Covid_19",
+                title: "Covid 19",
                 onClick: Navigator.of(context).pop),
             Text(
               Covid19Dummy.covid19Title,
               style: Theme.of(context).textTheme.headline1,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            CachedNetworkImage(
+              imageUrl:
+                  'https://pbs.twimg.com/profile_images/945853318273761280/0U40alJG_400x400.jpg',
+              imageBuilder: (context, imageProvider) => Center(
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: imageProvider, fit: BoxFit.cover),
+                  ),
+                ),
+              ),
+              placeholder: (context, url) => const CircularProgressIndicator(),
+              errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
             const SizedBox(
               height: 15,
